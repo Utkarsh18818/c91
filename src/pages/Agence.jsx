bc621f5123f2b6241a92c37dc5616403071c3cae
Utkarsh <Utkarsh190707@gmail.com>
2025-08-31 13:32:15 +0530

    integrated ScrollTrigger in Agence

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -1,8 +1,82 @@
-import React from 'react'
+import { useGSAP } from '@gsap/react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+import React, { useEffect, useRef } from 'react'
 
 const Agence = () => {
+  const imageDivRef = useRef(null);
+  const imageRef = useRef(null);
+  const imageArray = [
+    "https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Olivier_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Lawrence_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/HugoJoseph_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/ChantalG_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/MyleneS_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/SophieA_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Claire_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Michele_480X640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/MEL_480X640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/CAMILLE_480X640_2-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/MAXIME_480X640_2-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/MEGGIE_480X640_2-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/joel_480X640_3-480x640.jpg",
+  ]
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+  }, []);
+useGSAP(() => {
+    gsap.to(imageDivRef.current, {
+      y: 100, // Add some movement
+      duration: 1,
+      scrollTrigger: {  // Note: lowercase 's' in scrollTrigger
+        trigger: imageDivRef.current,
+        start: "top 5%",
+        end: "top -97%",
+        pin: true,
+        scrub: true,
+        toggleActions: "play none none reverse",
+        onUpdate:(e)=>{
+          let imageIndex;
+          if(e.progress < 1){
+            imageIndex = Math.floor(e.progress * imageArray.length);
+          }
+           else{
+            imageIndex = imageArray.length - 1;
+           }
+          imageRef.current.src = imageArray[imageIndex];
+        }
+      }
+    })
+  })
   return (
-    <div>Agence</div>
+    <div>
+    <div className="relative section1">
+      <div ref={imageDivRef} className='h-[25vw] w-[20vw] rounded-2xl absolute top-[7vw] left-[30vw] z-10 overflow-hidden'>
+        <img ref={imageRef}
+          src="https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg" 
+          alt="Team member" 
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div className='font-[font1] min-h-screen relative'>
+        <div className='absolute top-1/2 w-full z-20'>
+          <h1 className='text-[19vw] text-center uppercase leading-[15vw]'>
+            Soixan7e <br/>
+            Douze
+          </h1>
+          <div className='mt-8 w-full flex justify-end pr-8'>
+            <p className='text-3xl w-1/2 text-right leading-snug'>
+              We're inquisitive and open-minded, and we make sure creativity crowds out ego from every corner. A brand is a living thing, with values, a personality and a story. If we ignore that, we can achieve short-term success, but not influence that goes the distance. We bring that perspective to every brand story we help tell.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="section2 h-[200vh]">
+
+    </div>
+    </div>
   )
 }
 
